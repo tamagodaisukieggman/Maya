d37ef1ffc684c0008580df9067c45bd3675dcc54
tamagodaisukieggman <kesunyushi5728@gmail.com>
2023-11-13 23:14:59 +0900

    Update ikChain.py

--- scripts/tkgTools/launchers/maya/tkgTools/python/tkgRigBuild/build/parts/ikChain.py
+++ scripts/tkgTools/launchers/maya/tkgTools/python/tkgRigBuild/build/parts/ikChain.py
@@ -365,10 +365,10 @@ except:
                 back_spl = splited[2]
 
             for fsp in front_spl:
-                cmds.parent(ik_mid_ctrls[fsp].top, self.base_ctrl.ctrl)
+                cmds.parent(ik_mid_ctrls[fsp].top, self.ik_spline_base_up_joint_ctrl.ctrl)
 
             for bsp in back_spl:
-                cmds.parent(ik_mid_ctrls[bsp].top, self.main_ctrl.ctrl)
+                cmds.parent(ik_mid_ctrls[bsp].top, self.ik_spline_main_up_joint_ctrl.ctrl)
 
             if mid_spl:
                 cmds.pointConstraint(self.base_ctrl.ctrl, ik_mid_ctrls[mid_spl].top, w=True, mo=True)
@@ -394,6 +394,7 @@ except:
 
             for ik_spl, fk in zip(self.ik_chain.joints, self.ik_spline_fk.fk_ctrls):
                 cmds.connectAttr(ik_spl+'.r', fk.ctrl+'_SDK_GRP.r', f=True)
+                # cmds.connectAttr(ik_spl+'.' + self.stretchy_axis, fk.ctrl+'_SDK_GRP.' + self.stretchy_axis, f=True)
 
             poc_ik_fk = cmds.pointConstraint(self.base_ctrl.ctrl, self.ik_spline_fk.fk_top, w=True, mo=True)[0]
 
