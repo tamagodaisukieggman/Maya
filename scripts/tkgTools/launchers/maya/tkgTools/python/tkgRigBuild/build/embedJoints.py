23fdb3ab5b48ebf5a1b86b09da2c7309fa4e17cc
tamagodaisukieggman <kesunyushi5728@gmail.com>
2023-11-16 00:19:01 +0900

    embedの整理

--- scripts/tkgTools/launchers/maya/tkgTools/python/tkgRigBuild/build/embedJoints.py
+++ scripts/tkgTools/launchers/maya/tkgTools/python/tkgRigBuild/build/embedJoints.py
@@ -1,52 +1,15 @@
 # -*- coding: utf-8 -*-
 import maya.cmds as cmds
-import json
 
 import tkgRigBuild.libs.aim as tkgAim
 import tkgRigBuild.libs.modifyJoints as tkgMJ
 import tkgRigBuild.libs.control.ctrl as tkgCtrl
+import tkgRigBuild.libs.common as tkgCommon
 reload(tkgAim)
 reload(tkgMJ)
 reload(tkgCtrl)
+reload(tkgCommon)
 
-
-def create_joint(name=None, position=None):
-    joint = cmds.createNode( 'joint' , name=name )
-    cmds.xform( joint , worldSpace=True , translation=position )
-    return joint
-
-# This method creates a few joints to see the embedding.
-def create_joints_from_embedding( embedding ):
-    embedded_joints = [create_joint(name=name, position=position)
-                        for name , position in embedding[ 'joints' ].items( )]
-    return embedded_joints
-
-def get_mid_point(pos1, pos2, percentage=0.5):
-    mid_point = [pos1[0] + (pos2[0] - pos1[0]) * percentage,
-                 pos1[1] + (pos2[1] - pos1[1]) * percentage,
-                 pos1[2] + (pos2[2] - pos1[2]) * percentage]
-    return mid_point
-
-def add_segments(start_position=None, end_position=None, base_name=None, count=3, embedding=None):
-    # base_name = 'spine'
-    # count = 3
-    step = 1.0 / count
-    max_rate = 1 - step
-    rate = 0
-    for i in range(count):
-        mid_point = get_mid_point(start_position, end_position, rate)
-        embedding['joints']['{}_{}'.format(base_name, str(i+1).zfill(2))] = mid_point
-        rate += step
-
-def merge_joints(joints):
-    for obj in joints:
-        set_wr = cmds.xform(obj, q=1, ro=1, ws=1)
-        cmds.setAttr('{}.jo'.format(obj), *(0, 0, 0))
-        cmds.xform(obj, ro=set_wr, ws=1, a=1)
-
-def freeze_rotate(joints):
-    [cmds.makeIdentity(obj, n=False, s=False, r=True, t=False, apply=True, pn=True)
-        for obj in joints]
 
 def create_offset_grp(obj=None):
     obj_grp = cmds.createNode('transform', n=obj+'_GRP', ss=True)
@@ -89,206 +52,10 @@ def create_pos_rot_locs(objects=None):
 
 # First select the shape, not the transform.
 sel = cmds.ls(os=True)
-shape = cmds.listRelatives(sel[0], s=True)[0]
-cmds.select( shape , r=True )
+segments = tkgMJ.embed_biped_joints(sel[0], root_count=1, spine_count=3, neck_count=2, knee_count=2)
+root_segments, spine_segments, neck_segments, left_knee_segments, right_knee_segments
 
-result = cmds.skeletonEmbed()
-embedding = json.loads( result )
-
-# Exchange keys
-embedding['joints']['left_arm'] = embedding['joints'].pop('left_shoulder')
-embedding['joints']['right_arm'] = embedding['joints'].pop('right_shoulder')
-
-embedding['joints']['left_ball'] = embedding['joints'].pop('left_foot')
-embedding['joints']['right_ball'] = embedding['joints'].pop('right_foot')
-
-
-# Fixing spine
-embedding['joints']['spine'] = embedding['joints'].pop('back')
-
-# Fixing arm and shoulder
-shoulders_position = embedding['joints']['shoulders']
-left_arm_position = embedding['joints']['left_arm']
-right_arm_position = embedding['joints']['right_arm']
-
-left_arm_mid_point = get_mid_point(shoulders_position, left_arm_position)
-right_arm_mid_point = get_mid_point(shoulders_position, right_arm_position)
-
-embedding['joints'].update({'left_shoulder':left_arm_mid_point})
-embedding['joints'].update({'right_shoulder':right_arm_mid_point})
-
-# Fixing neck
-embedding['joints']['neck'] = embedding['joints'].pop('shoulders')
-neck_position = embedding['joints']['neck']
-head_position = embedding['joints']['head']
-
-neck_head_mid_point = get_mid_point(neck_position, head_position, 0.3)
-embedding['joints']['neck'] = neck_head_mid_point
-
-# Fixing head
-fix_head_mid_point = get_mid_point(neck_position, head_position, 0.7)
-embedding['joints']['head'] = fix_head_mid_point
-
-# Fixing Side
-left_sides = {}
-for joint, position in embedding['joints'].items():
-    if 'left_' in joint:
-        left_sides[joint] = position
-
-for joint, position in embedding['joints'].items():
-    if 'left_' in joint:
-        l_pos = left_sides[joint]
-        r_pos = []
-        r_pos.append(l_pos[0]*-1)
-        r_pos.append(l_pos[1])
-        r_pos.append(l_pos[2])
-        embedding['joints'][joint.replace('left_', 'right_')] = r_pos
-
-# Fixing Straight
-for s in ['left_', 'right_']:
-    for i, p in enumerate([['arm', 'elbow', 'hand'], ['thigh', 'knee', 'ankle']]):
-        start_position = embedding['joints'][s+p[0]]
-        end_position = embedding['joints'][s+p[2]]
-        embedding['joints'][s+p[1]] = get_mid_point(start_position, end_position)
-
-for joint in ['hips', 'spine', 'neck', 'head']:
-    old_pos = embedding['joints'][joint]
-    new_pos = []
-    new_pos.append(old_pos[0]*0)
-    new_pos.append(old_pos[1])
-    new_pos.append(old_pos[2])
-    embedding['joints'][joint] = new_pos
-
-# Add Spine Segments
-spine_count=3
-if spine_count != 1:
-    start_position = embedding['joints']['hips']
-    end_position = embedding['joints']['neck']
-    add_segments(start_position=start_position,
-                 end_position=end_position,
-                 base_name='spine',
-                 count=spine_count,
-                 embedding=embedding)
-    embedding['joints'].pop('spine')
-
-# Add Neck Segments
-neck_count=2
-if neck_count != 1:
-    start_position = embedding['joints']['neck']
-    end_position = embedding['joints']['head']
-    add_segments(start_position=start_position,
-                 end_position=end_position,
-                 base_name='neck',
-                 count=neck_count,
-                 embedding=embedding)
-    embedding['joints'].pop('neck')
-
-# Add Root Joint
-embedding['joints']['root'] = [0, 0, 0]
-
-# Add Neck Segments
-root_count=1
-if root_count != 1:
-    start_position = embedding['joints']['root']
-    end_position = embedding['joints']['root']
-    add_segments(start_position=start_position,
-                 end_position=end_position,
-                 base_name='root',
-                 count=root_count,
-                 embedding=embedding)
-    embedding['joints'].pop('root')
-
-
-# Connect Joints
 mirror = ['left_', 'right_']
-parent_hierarchy = {
-    'hips':'root',
-    'spine_01':'hips',
-    'spine_02':'spine_01',
-    'spine_03':'spine_02',
-    'neck_01':'spine_03',
-    'neck_02':'neck_01',
-    'head':'neck_02',
-
-    'left_hand':'left_elbow',
-    'left_elbow':'left_arm',
-    'left_arm':'left_shoulder',
-    'left_shoulder':'spine_03',
-
-    'right_hand':'right_elbow',
-    'right_elbow':'right_arm',
-    'right_arm':'right_shoulder',
-    'right_shoulder':'spine_03',
-
-    'left_ball':'left_ankle',
-    'left_ankle':'left_knee',
-    'left_knee':'left_thigh',
-    'left_thigh':'hips',
-
-    'right_ball':'right_ankle',
-    'right_ankle':'right_knee',
-    'right_knee':'right_thigh',
-    'right_thigh':'hips',
-
-}
-
-# create joints
-# 実際に作成するのはembeddingに格納した値を確認してから
-embedded_joints = create_joints_from_embedding( embedding )
-
-for child, parent in parent_hierarchy.items():
-    cmds.parent(child, parent)
-
-# Aiming Spine
-spine_01 = 'spine_01'
-tkgMJ.aim_correct_joints(sel=[spine_01],
-                       aim_axis='y',
-                       up_axis='y',
-                       worldUpType='object',
-                       ssc_sts=False,
-                       worldSpace=True,
-                       world_axis='y')
-
-# Aiming shoulder
-left_shoulder = 'left_shoulder'
-tkgMJ.aim_correct_joints(sel=[left_shoulder],
-                       aim_axis='x',
-                       up_axis='-y',
-                       worldUpType='object',
-                       ssc_sts=False,
-                       worldSpace=True,
-                       world_axis='y')
-
-# Aiming arm
-left_arm = 'left_arm'
-tkgMJ.aim_correct_joints(sel=[left_arm],
-                       aim_axis='x',
-                       up_axis='-y',
-                       worldUpType='object',
-                       ssc_sts=False,
-                       worldSpace=True,
-                       world_axis='x')
-
-# Aiming arm
-left_thigh = 'left_thigh'
-tkgMJ.aim_correct_joints(sel=[left_thigh],
-                       aim_axis='x',
-                       up_axis='y',
-                       worldUpType='object',
-                       ssc_sts=False,
-                       worldSpace=True,
-                       world_axis='x')
-
-# Mirror Joints
-cmds.delete('right_shoulder')
-cmds.delete('right_thigh')
-
-left_parts = [left_shoulder, left_thigh]
-for obj in left_parts:
-    mirror_joints = cmds.mirrorJoint(obj,
-                 mirrorYZ=True,
-                 mirrorBehavior=True,
-                 searchReplace=mirror)
 
 # Mirror Joints
 root_jnt = 'root'
@@ -348,7 +115,7 @@ cmds.parent('left_hand_POS_LOC_GRP', 'left_arm_POS_LOC')
 cmds.parent('left_ankle_POS_LOC_GRP', 'left_thigh_POS_LOC')
 cmds.parent('left_ball_POS_LOC_GRP', 'left_thigh_POS_LOC')
 
-# Aim
+# # Aim
 left_arm_aim_loc = left_arm_pos_locs[0]+'_AIM'
 cmds.spaceLocator(n=left_arm_aim_loc)
 aim_loc_grp = create_offset_grp(left_arm_aim_loc)
@@ -362,13 +129,60 @@ cmds.aimConstraint(
     upVector=[0,-1,0],
     worldUpType='scene',
 )
+cmds.setAttr(left_arm_aim_loc+'.v', 0)
 
-
-# Point Const
+# # # Point Const
 cmds.pointConstraint('left_shoulder_POS_LOC_AIM', 'left_hand_POS_LOC_OFFSET_GRP', w=True, mo=True)
 
 cmds.pointConstraint('left_arm_POS_LOC', 'left_elbow_POS_LOC_OFFSET_GRP', w=True, mo=True)
 cmds.pointConstraint('left_hand_POS_LOC', 'left_elbow_POS_LOC_OFFSET_GRP', w=True, mo=True)
+
+# Scale Tweak
+# arm
+cmds.addAttr('left_shoulder_POS_LOC_GRP', ln='posLocsScale', at='double', dv=1, k=True)
+cmds.addAttr('left_shoulder_POS_LOC_GRP', ln='rotLocsScale', at='double', dv=1, k=True)
+
+for lapl, larl in zip(left_arm_pos_locs, left_arm_rot_locs):
+    cmds.connectAttr('left_shoulder_POS_LOC_GRP.posLocsScale', lapl+'.localScaleX', f=True)
+    cmds.connectAttr('left_shoulder_POS_LOC_GRP.posLocsScale', lapl+'.localScaleY', f=True)
+    cmds.connectAttr('left_shoulder_POS_LOC_GRP.posLocsScale', lapl+'.localScaleZ', f=True)
+
+    cmds.connectAttr('left_shoulder_POS_LOC_GRP.rotLocsScale', larl+'.sx', f=True)
+    cmds.connectAttr('left_shoulder_POS_LOC_GRP.rotLocsScale', larl+'.sy', f=True)
+    cmds.connectAttr('left_shoulder_POS_LOC_GRP.rotLocsScale', larl+'.sz', f=True)
+
+# leg
+cmds.addAttr('left_thigh_POS_LOC_GRP', ln='posLocsScale', at='double', dv=1, k=True)
+cmds.addAttr('left_thigh_POS_LOC_GRP', ln='rotLocsScale', at='double', dv=1, k=True)
+
+for llpl, llrl in zip(left_leg_pos_locs, left_leg_rot_locs):
+    cmds.connectAttr('left_thigh_POS_LOC_GRP.posLocsScale', llpl+'.localScaleX', f=True)
+    cmds.connectAttr('left_thigh_POS_LOC_GRP.posLocsScale', llpl+'.localScaleY', f=True)
+    cmds.connectAttr('left_thigh_POS_LOC_GRP.posLocsScale', llpl+'.localScaleZ', f=True)
+
+    cmds.connectAttr('left_thigh_POS_LOC_GRP.rotLocsScale', llrl+'.sx', f=True)
+    cmds.connectAttr('left_thigh_POS_LOC_GRP.rotLocsScale', llrl+'.sy', f=True)
+    cmds.connectAttr('left_thigh_POS_LOC_GRP.rotLocsScale', llrl+'.sz', f=True)
+
+
+# Adjustment Grp
+adjustment_grp = 'adjustment_grp'
+cmds.createNode('transform', n=adjustment_grp, ss=True)
+cmds.parent(mirror_grp, adjustment_grp)
+cmds.parent('left_shoulder_POS_LOC_GRP', adjustment_grp)
+cmds.parent('left_thigh_POS_LOC_GRP', adjustment_grp)
+cmds.parent(aim_loc_grp, adjustment_grp)
+
+cmds.addAttr(adjustment_grp, ln='armPosLocsScale', at='double', dv=1, k=True)
+cmds.addAttr(adjustment_grp, ln='armRotLocsScale', at='double', dv=1, k=True)
+cmds.connectAttr(adjustment_grp + '.armPosLocsScale', 'left_shoulder_POS_LOC_GRP.posLocsScale', f=True)
+cmds.connectAttr(adjustment_grp + '.armRotLocsScale', 'left_shoulder_POS_LOC_GRP.rotLocsScale', f=True)
+
+cmds.addAttr(adjustment_grp, ln='legPosLocsScale', at='double', dv=1, k=True)
+cmds.addAttr(adjustment_grp, ln='legRotLocsScale', at='double', dv=1, k=True)
+cmds.connectAttr(adjustment_grp + '.legPosLocsScale', 'left_thigh_POS_LOC_GRP.posLocsScale', f=True)
+cmds.connectAttr(adjustment_grp + '.legRotLocsScale', 'left_thigh_POS_LOC_GRP.rotLocsScale', f=True)
+
 
 #--------------------------
 # Adjusting Axis
@@ -497,7 +311,7 @@ set_leg_axis_pv_up(leg_aim_axis='x', leg_up_axis='z',
             ball_aim_axis='-x', ball_up_axis='z')
 
 # delete guides
-cnsts = [n for n in cmds.ls('root', dag=True) if 'Constraint' in n]
-[cmds.delete(cn) for cn in cnsts]
-grps = cmds.ls('*_GRP', assemblies=True)
-[cmds.delete(gp) for gp in grps]
+# cnsts = [n for n in cmds.ls('root', dag=True) if 'Constraint' in n]
+# [cmds.delete(cn) for cn in cnsts]
+# grps = cmds.ls('*_GRP', assemblies=True)
+# [cmds.delete(gp) for gp in grps]
--- scripts/tkgTools/launchers/maya/tkgTools/python/tkgRigBuild/libs/modifyJoints.py
+++ scripts/tkgTools/launchers/maya/tkgTools/python/tkgRigBuild/libs/modifyJoints.py
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
 import maya.cmds as cmds
 from imp import reload
+import json
 import math
 
 import tkgRigBuild.libs.aim as tkgAim
+import tkgRigBuild.libs.common as tkgCommon
 reload(tkgAim)
+reload(tkgCommon)
 
 def truncate(f, n):
     return math.floor(f * 10 ** n) / 10 ** n
@@ -134,6 +137,365 @@ def aim_correct_joints(sel=None,
                  world_axis=world_axis)
     correct_joints(sel=cmds.ls(os=True, dag=True),
                      ssc_sts=ssc_sts)
+
+def create_joint(name=None, position=None):
+    joint = cmds.createNode('joint' , name=name)
+    cmds.xform(joint ,worldSpace=True, translation=position)
+    return joint
+
+def create_joints_from_embedding(embedding):
+    embedded_joints = [create_joint(name=name, position=position)
+                        for name , position in embedding['joints'].items()]
+    return embedded_joints
+
+def add_segments(start_position=None, end_position=None, base_name=None, count=3, embedding=None):
+    # base_name = 'spine'
+    # count = 3
+    seg_joints = []
+    step = 1.0 / count
+    max_rate = 1 - step
+    rate = 0
+    for i in range(count):
+        mid_point = tkgCommon.get_mid_point(start_position, end_position, rate)
+        seg_jnt = '{}_{}'.format(base_name, str(i+1).zfill(2))
+        seg_joints.append(seg_jnt)
+        embedding['joints'][seg_jnt] = mid_point
+        rate += step
+
+    return seg_joints
+
+class Segments:
+    def __init__(self,
+                 start=None,
+                 end=None,
+                 base_name=None,
+                 count=None,
+                 embedding=None,
+                 parent=None):
+        self.start = start
+        self.end = end
+        self.base_name = base_name
+        self.count = count
+        self.embedding = embedding
+        self.parent = parent
+
+        self.top = None
+        self.bottom = None
+        self.parent_dict = {}
+
+        self.insert()
+
+    def insert(self):
+        if self.count != 1:
+            start_position = self.embedding['joints'][self.start]
+            end_position = self.embedding['joints'][self.end]
+            seg_joints = add_segments(start_position=start_position,
+                                     end_position=end_position,
+                                     base_name=self.base_name,
+                                     count=self.count,
+                                     embedding=self.embedding)
+            self.embedding['joints'].pop(self.base_name)
+
+            self.top = seg_joints[0]
+            self.bottom = seg_joints[-1]
+
+            # set parent
+            if self.parent:
+                self.parent_dict[self.top] = self.parent
+            for i, seg_j in enumerate(seg_joints):
+                if i == 0:
+                    pass
+                else:
+                    self.parent_dict[seg_j] = seg_joints[i-1]
+
+        else:
+            if self.parent:
+                self.parent_dict[self.start] = self.parent
+
+            self.top = self.base_name
+            self.bottom = self.base_name
+
+
+def embed_biped_joints(mesh=None, root_count=1, spine_count=3, neck_count=1, knee_count=1):
+    shape = tkgCommon.get_shapes(mesh)[0]
+
+    result = cmds.skeletonEmbed(shape)
+    embedding = json.loads(result)
+
+    # Exchange keys
+    embedding['joints']['left_arm'] = embedding['joints'].pop('left_shoulder')
+    embedding['joints']['right_arm'] = embedding['joints'].pop('right_shoulder')
+
+    embedding['joints']['left_ball'] = embedding['joints'].pop('left_foot')
+    embedding['joints']['right_ball'] = embedding['joints'].pop('right_foot')
+
+
+    # Fixing spine
+    embedding['joints']['spine'] = embedding['joints'].pop('back')
+
+    # Fixing arm and shoulder
+    shoulders_position = embedding['joints']['shoulders']
+    left_arm_position = embedding['joints']['left_arm']
+    right_arm_position = embedding['joints']['right_arm']
+
+    left_arm_mid_point = tkgCommon.get_mid_point(shoulders_position, left_arm_position)
+    right_arm_mid_point = tkgCommon.get_mid_point(shoulders_position, right_arm_position)
+
+    embedding['joints'].update({'left_shoulder':left_arm_mid_point})
+    embedding['joints'].update({'right_shoulder':right_arm_mid_point})
+
+    # Fixing neck
+    embedding['joints']['neck'] = embedding['joints'].pop('shoulders')
+    neck_position = embedding['joints']['neck']
+    head_position = embedding['joints']['head']
+
+    neck_head_mid_point = tkgCommon.get_mid_point(neck_position, head_position, 0.3)
+    embedding['joints']['neck'] = neck_head_mid_point
+
+    # Fixing head
+    fix_head_mid_point = tkgCommon.get_mid_point(neck_position, head_position, 0.7)
+    embedding['joints']['head'] = fix_head_mid_point
+
+    # Fixing Side
+    left_sides = {}
+    for joint, position in embedding['joints'].items():
+        if 'left_' in joint:
+            left_sides[joint] = position
+
+    for joint, position in embedding['joints'].items():
+        if 'left_' in joint:
+            l_pos = left_sides[joint]
+            r_pos = []
+            r_pos.append(l_pos[0]*-1)
+            r_pos.append(l_pos[1])
+            r_pos.append(l_pos[2])
+            embedding['joints'][joint.replace('left_', 'right_')] = r_pos
+
+    # Fixing Straight
+    for s in ['left_', 'right_']:
+        for i, p in enumerate([['arm', 'elbow', 'hand'], ['thigh', 'knee', 'ankle']]):
+            start_position = embedding['joints'][s+p[0]]
+            end_position = embedding['joints'][s+p[2]]
+            if 'thigh' in p:
+                percentage = 0.5
+            elif 'arm' in p:
+                percentage = 0.4
+            embedding['joints'][s+p[1]] = tkgCommon.get_mid_point(start_position, end_position, percentage)
+
+    # Fix Hand Ankle
+    for s in ['left_', 'right_']:
+        start_position = embedding['joints'][s+'hand']
+        end_position = embedding['joints'][s+'arm']
+        embedding['joints'][s+'hand'] = tkgCommon.get_mid_point(start_position, end_position, 0.25)
+
+        start_position = embedding['joints'][s+'ankle']
+        end_position = embedding['joints'][s+'thigh']
+        embedding['joints'][s+'ankle'] = tkgCommon.get_mid_point(start_position, end_position, 0.07)
+
+    # Fix Elbow Knee
+    for s in ['left_', 'right_']:
+        start_position = embedding['joints'][s+'hand']
+        mid_position = embedding['joints'][s+'elbow']
+        end_position = embedding['joints'][s+'arm']
+        mid_point = tkgCommon.get_mid_point(start_position, end_position, 0.8)
+        embedding['joints'][s+'elbow'] = [mid_position[0], mid_position[1], mid_point[2]]
+
+        # start_position = embedding['joints'][s+'ankle']
+        # mid_position = embedding['joints'][s+'knee']
+        # end_position = embedding['joints'][s+'thigh']
+        # mid_point = tkgCommon.get_mid_point(start_position, end_position, 1.000005)
+        # embedding['joints'][s+'knee'] = [mid_position[0], mid_position[1], mid_point[2]]
+
+
+    for joint in ['hips', 'spine', 'neck', 'head']:
+        old_pos = embedding['joints'][joint]
+        new_pos = []
+        new_pos.append(old_pos[0]*0)
+        new_pos.append(old_pos[1])
+        new_pos.append(old_pos[2])
+        embedding['joints'][joint] = new_pos
+
+    # Add Root Joint
+    embedding['joints']['root'] = [0, 0, 0]
+
+    # Add Root Segments
+    # root_count=1
+    # if root_count != 1:
+    #     start_position = embedding['joints']['root']
+    #     end_position = embedding['joints']['root']
+    #     add_segments(start_position=start_position,
+    #                  end_position=end_position,
+    #                  base_name='root',
+    #                  count=root_count,
+    #                  embedding=embedding)
+    #     embedding['joints'].pop('root')
+
+    root_segments = Segments(start='root',
+                        end='root',
+                        base_name='root',
+                        count=root_count,
+                        embedding=embedding,
+                        parent=None)
+
+    # Add Spine Segments
+    # spine_count=3
+    # if spine_count != 1:
+    #     start_position = embedding['joints']['hips']
+    #     end_position = embedding['joints']['neck']
+    #     add_segments(start_position=start_position,
+    #                  end_position=end_position,
+    #                  base_name='spine',
+    #                  count=spine_count,
+    #                  embedding=embedding)
+    #     embedding['joints'].pop('spine')
+
+    spine_segments = Segments(start='hips',
+                        end='neck',
+                        base_name='spine',
+                        count=spine_count,
+                        embedding=embedding,
+                        parent='hips')
+
+    # Add Neck Segments
+    # neck_count=2
+    # if neck_count != 1:
+    #     start_position = embedding['joints']['neck']
+    #     end_position = embedding['joints']['head']
+    #     add_segments(start_position=start_position,
+    #                  end_position=end_position,
+    #                  base_name='neck',
+    #                  count=neck_count,
+    #                  embedding=embedding)
+    #     embedding['joints'].pop('neck')
+
+    neck_segments = Segments(start='neck',
+                        end='head',
+                        base_name='neck',
+                        count=neck_count,
+                        embedding=embedding,
+                        parent=spine_segments.bottom)
+
+    # Add Knee Segments
+    left_knee_segments = Segments(start='left_knee',
+                        end='left_ankle',
+                        base_name='left_knee',
+                        count=knee_count,
+                        embedding=embedding,
+                        parent='left_thigh')
+
+    right_knee_segments = Segments(start='right_knee',
+                        end='right_ankle',
+                        base_name='right_knee',
+                        count=knee_count,
+                        embedding=embedding,
+                        parent='right_thigh')
+
+
+    # Connect Joints
+    mirror = ['left_', 'right_']
+    parent_hierarchy = {
+        'hips':root_segments.bottom,
+        'head':neck_segments.bottom,
+        spine_segments.top:'hips',
+
+        'left_hand':'left_elbow',
+        'left_elbow':'left_arm',
+        'left_arm':'left_shoulder',
+        'left_shoulder':spine_segments.bottom,
+
+        'right_hand':'right_elbow',
+        'right_elbow':'right_arm',
+        'right_arm':'right_shoulder',
+        'right_shoulder':spine_segments.bottom,
+
+        'left_thigh':'hips',
+        left_knee_segments.top:'left_thigh',
+        'left_ankle':left_knee_segments.bottom,
+        'left_ball':'left_ankle',
+
+        'right_thigh':'hips',
+        right_knee_segments.top:'right_thigh',
+        'right_ankle':right_knee_segments.bottom,
+        'right_ball':'right_ankle',
+
+    }
+
+    for child, parent in root_segments.parent_dict.items():
+        parent_hierarchy[child] = parent
+
+    for child, parent in spine_segments.parent_dict.items():
+        parent_hierarchy[child] = parent
+
+    for child, parent in neck_segments.parent_dict.items():
+        parent_hierarchy[child] = parent
+
+    for child, parent in left_knee_segments.parent_dict.items():
+        parent_hierarchy[child] = parent
+
+    for child, parent in right_knee_segments.parent_dict.items():
+        parent_hierarchy[child] = parent
+
+    # create joints
+    # 実際に作成するのはembeddingに格納した値を確認してから
+    embedded_joints = create_joints_from_embedding( embedding )
+
+    for child, parent in parent_hierarchy.items():
+        if child and parent:
+            cmds.parent(child, parent)
+
+
+    # Aiming Spine
+    spine_01 = 'spine_01'
+    aim_correct_joints(sel=[spine_01],
+                           aim_axis='y',
+                           up_axis='y',
+                           worldUpType='object',
+                           ssc_sts=False,
+                           worldSpace=True,
+                           world_axis='y')
+
+    # Aiming shoulder
+    left_shoulder = 'left_shoulder'
+    aim_correct_joints(sel=[left_shoulder],
+                           aim_axis='x',
+                           up_axis='-y',
+                           worldUpType='object',
+                           ssc_sts=False,
+                           worldSpace=True,
+                           world_axis='y')
+
+    # Aiming arm
+    left_arm = 'left_arm'
+    aim_correct_joints(sel=[left_arm],
+                           aim_axis='x',
+                           up_axis='-y',
+                           worldUpType='object',
+                           ssc_sts=False,
+                           worldSpace=True,
+                           world_axis='x')
+
+    # Aiming arm
+    left_thigh = 'left_thigh'
+    aim_correct_joints(sel=[left_thigh],
+                           aim_axis='x',
+                           up_axis='y',
+                           worldUpType='object',
+                           ssc_sts=False,
+                           worldSpace=True,
+                           world_axis='x')
+
+    # Mirror Joints
+    cmds.delete('right_shoulder')
+    cmds.delete('right_thigh')
+
+    left_parts = [left_shoulder, left_thigh]
+    for obj in left_parts:
+        mirror_joints = cmds.mirrorJoint(obj,
+                     mirrorYZ=True,
+                     mirrorBehavior=True,
+                     searchReplace=mirror)
+
+    return root_segments, spine_segments, neck_segments, left_knee_segments, right_knee_segments
 
 # wizard2仕様
 def joint_labeling():
