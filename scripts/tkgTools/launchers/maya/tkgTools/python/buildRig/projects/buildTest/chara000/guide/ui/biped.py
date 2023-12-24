a57e34c29064b259a5c8bb7c9a11a5a5e4516de0
tamagodaisukieggman <kesunyushi5728@gmail.com>
2023-12-25 01:43:42 +0900

    Update biped.py

--- scripts/tkgTools/launchers/maya/tkgTools/python/buildRig/projects/buildTest/chara000/guide/ui/biped.py
+++ scripts/tkgTools/launchers/maya/tkgTools/python/buildRig/projects/buildTest/chara000/guide/ui/biped.py
@@ -1,10 +1,16 @@
 # -*- coding: utf-8 -*-
 from imp import reload
+import os
 
-import buildRig.libs.ui as guide_ui
+import buildRig.libs.picker.ui as guide_ui
 reload(guide_ui)
 
 class GuideUI:
     def __init__(self):
         self.picker = guide_ui.PickerAnimTools()
+        self.picker.buildUI()
         self.picker.show(dockable=True)
+
+def onMayaDroppedPythonFile(*args):
+    guideUI = GuideUI()
+
