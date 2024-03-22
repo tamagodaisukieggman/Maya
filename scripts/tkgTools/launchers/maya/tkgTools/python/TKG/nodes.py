c7474ca93ce039d5f6d4942d77e1b49bda24f740
tamagodaisukieggman <kesunyushi5728@gmail.com>
2024-03-23 03:39:02 +0900

    Summery

---
+++ scripts/tkgTools/launchers/maya/tkgTools/python/TKG/nodes.py
@@ -0,0 +1,75 @@
+﻿# -*- coding: utf-8 -*-
+import re
+
+import maya.cmds as cmds
+
+"""
+よく使用するスクリプト
+# selection
+nodes = cmds.ls(os=True, fl=True) or []
+
+"""
+# rename
+def rename(obj=None, prefix=None, suffix=None, replace=None):
+    """
+    obj=None, prefix=None, suffix=None, replace=None
+    """
+    # replace
+    replace_name = obj
+    if replace:
+        replace_name = obj.replace(*replace)
+
+    # prefix
+    if not prefix:
+        prefix = ''
+    prefix_name = re.sub('^', prefix, replace_name)
+
+    # suffix
+    if not suffix:
+        suffix = ''
+    renamed = re.sub('$', suffix, prefix_name)
+
+    return renamed
+
+# virtual rename
+def virtual_reames(names=None, p='', s='', r=['', '']):
+    """
+    仮想のリネームを取得
+    """
+    return [rename(n, p, s, r) for n in names]
+
+# duplicate
+class Duplicate:
+    """
+    dup = Duplicate(nodes, '', '', ['BIND_', 'IK_'])
+    dups = dup.duplicate()
+    """
+    def __init__(self, nodes=None, prefix=None, suffix=None, replace=None, hierarchy=None):
+        if nodes:
+            self.nodes = nodes
+        else:
+            self.nodes = cmds.ls(os=True, fl=True) or []
+        self.prefix = prefix
+        self.suffix = suffix
+        self.replace = replace
+        self.hierarchy = hierarchy
+        self.virtuals = None
+
+        self.virtual_reames()
+
+    def virtual_reames(self):
+        if self.hierarchy:
+            self.nodes = cmds.ls(self.nodes, dag=True)
+
+        self.virtuals = virtual_reames(self.nodes,
+                              self.prefix,
+                              self.suffix,
+                              self.replace)
+
+    def duplicate(self):
+        if self.hierarchy:
+            dups = cmds.duplicate(self.nodes, rc=True)
+        else:
+            dups = cmds.duplicate(self.nodes, rc=True, po=True)
+
+        return [cmds.rename(d, rslt) for d, rslt in zip(dups, self.virtuals)]
