b7db82c08f527e5a036fc7b316806985a13bfa23
tamagodaisukieggman <kesunyushi5728@gmail.com>
2024-03-23 22:52:13 +0900

    Summery

---
+++ scripts/tkgTools/launchers/maya/tkgTools/python/TKG/modules/baseModule.py
@@ -0,0 +1,36 @@
+﻿# -*- coding: utf-8 -*-
+from imp import reload
+
+import maya.cmds as cmds
+
+class Module:
+    def __init__(self, module=None, side=None):
+        if not module:
+            self.module = 'default'
+        else:
+            self.module = module
+
+        if not side:
+            self.side = 'Cn'
+        else:
+            self.side = side
+
+        self.module_name = '{}_{}_MODULE'.format(self.module, self.side)
+
+        self.rig_top = 'RIG'
+        self.base_rig_grps = ['MODULES', 'MODEL', 'SKEL']
+        self.rig_modules = self.base_rig_grps[0]
+        self.rig_model = self.base_rig_grps[1]
+        self.rig_skel = self.base_rig_grps[2]
+
+        if not cmds.objExists(self.rig_top):
+            cmds.createNode('transform', n=self.rig_top, ss=True)
+
+        for n in self.base_rig_grps:
+            if not cmds.objExists(n):
+                cmds.createNode('transform', n=n, ss=True)
+                cmds.parent(n, self.rig_top)
+
+    def create(self):
+        if not cmds.objExists(self.module_name):
+            cmds.createNode('transform', n=self.module_name, ss=True)
--- scripts/tkgTools/launchers/maya/tkgTools/python/TKG/modules/ikModule.py
+++ scripts/tkgTools/launchers/maya/tkgTools/python/TKG/modules/ikModule.py
@@ -6,17 +6,27 @@ import maya.cmds as cmds
 import TKG.library.rigJoints as tkgRigJoints
 import TKG.library.ik as tkgIk
 import TKG.library.control.ctrls as tkgCtrls
+import TKG.modules.baseModule as tkgModules
 reload(tkgRigJoints)
 reload(tkgIk)
 reload(tkgCtrls)
+reload(tkgModules)
 
-def create_ik_limb_module(nodes=None):
-    if not nodes:
-        nodes = cmds.ls(os=True, fl=True) or []
-    ik_joints = tkgRigJoints.create_ik_joints(nodes=nodes)
+class Build(tkgModules.Module):
+    def __init__(self, module=None, side=None):
+        super().__init__(module, side)
+        # self.module = module
+        # self.side = side
 
-    ikh = tkgIk.create_RP_ikHandle(ik_joints[0], ik_joints[-1])
+    def create_ik_limb_module(self, nodes=None, pv_idx=1):
+        if not nodes:
+            nodes = cmds.ls(os=True, fl=True) or []
+        ik_joints = tkgRigJoints.create_ik_joints(nodes=nodes)
 
-    ikBase_ctrl, ikBase_offset = tkgCtrls.create_ikBase_ctrl(node=nodes[0], axis=[0,0,0], scale=1)
+        ikh = tkgIk.create_RP_ikHandle(ik_joints[0], ik_joints[-1])
 
-    ikMain_ctrl, ikMain_offset = tkgCtrls.create_ikMain_ctrl(node=nodes[-1], axis=[0,0,0], scale=1)
+        ikBase_ctrl, ikBase_offset = tkgCtrls.create_ikBase_ctrl(node=ik_joints[0], axis=[0,0,0], scale=1)
+
+        ikMain_ctrl, ikMain_offset = tkgCtrls.create_ikMain_ctrl(node=ik_joints[-1], axis=[0,0,0], scale=1)
+
+        ikPv_ctrl, ikPv_offset = tkgCtrls.create_ikPv_ctrl(node=ik_joints[pv_idx], axis=[0,0,0], scale=1)
