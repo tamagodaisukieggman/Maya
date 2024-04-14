64772678e55b25c797d5d35f1bb1c91f9a603b11
tamagodaisukieggman <kesunyushi5728@gmail.com>
2024-04-14 22:37:17 +0900

    Summery

--- scripts/tkgTools/launchers/maya/tkgTools/python/TKG/build/testBuild.py
+++ scripts/tkgTools/launchers/maya/tkgTools/python/TKG/build/testBuild.py
@@ -1,22 +1,33 @@
 ﻿# -*- coding: utf-8 -*-
 from imp import reload
-import TKG.library.rigJoints as tkgRigJoints
-import TKG.library.ik as tkgIk
-reload(tkgRigJoints)
-reload(tkgIk)
 
-limb_joints = tkgRigJoints.create_limb_joints(nodes=None,
-                       blend_prefix='',
-                       blend_suffix='',
-                       blend_replace=['BIND_', 'BLEND_'],
-                       first_segments_num=8,
-                       second_segments_num=8,
-                       fk_prefix='',
-                       fk_suffix='',
-                       fk_replace=['BIND_', 'FK_'],
-                       ik_prefix='',
-                       ik_suffix='',
-                       ik_replace=['BIND_', 'IK_'])
+######################################
+# For Limb
+######################################
+# ik module
+import TKG.modules.ikModule as tkgIkModule
+reload(tkgIkModule)
 
+ikMod = tkgIkModule.Build('Lt', 'arm')
+ikMod.create_ik_limb()
 
-tkgIk.create_RP_ikHandle(limb_joints[2][0], limb_joints[2][-1])
+# fk module
+import TKG.modules.fkModule as tkgFkModule
+reload(tkgFkModule)
+
+fkMod = tkgFkModule.Build('Lt', 'arm')
+fkMod.create_fk_limb()
+
+# bendy module
+import TKG.modules.bendyModule as tkgBendyModule
+reload(tkgBendyModule)
+
+bendyMod = tkgBendyModule.Build('Lt', 'arm')
+bendyMod.create_bendy_limb()
+
+# blend module
+import TKG.modules.blendModule as tkgBlendModule
+reload(tkgBlendModule)
+
+blendMod = tkgBlendModule.Build('Lt', 'arm')
+blendMod.create_blend_limb()
